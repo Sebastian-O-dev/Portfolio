621f0f07c79addd59c04f4c70d6b571d746f2767
Sebastian-O-dev <sebastianolechno@gmail.com>
2024-10-26 16:19:09 +0200

    roman-encoding-v-0-2
    roman-encoding-v-0-2
    
    Added a more universal set of rules and conditions.
    Added a "looping" system where if the user exceeds "Z", the script starts "shifting" again from "A".
    More UX firendly language.
    More organised code.
    
    To do:
    write a script where multiple words with spaces in between can be used to encode longer messages.

--- roman_encoding.py
+++ roman_encoding.py
@@ -1,32 +1,45 @@
-code = ""
+import time
 
-print("\\Enter = end of program\\")
+print("\nThis program will encode your message (1 word limit, please don't use white-space),\nby adding an (x) amount of +1 'shift' in value to each letter, changing it's position\nin the English alphabet by (x).\n")
+print("If you exceed the last letter of the alphabet, the coding is 'looped around' back to the\nfirst letter of the alphabet.\n")
+print("\\Enter = end of program\\\n")
 
 while True:
-    shift = input("How many +1 values are to be added to each letter in UniCode?: ")
-    if shift == "":
-        print("End of program")
+    
+    shift_input = input("How many +1 'shifts' are to be done to each letter in the message?: ").strip().replace(" ", "")
+
+    if shift_input == "26":
+        print("By selecting 26 as the 'shift', you aren't encoding the message. Try again.")
+        continue
+
+    if shift_input == "":
+        print("\nEnd of program.")
         break
-    elif shift.isdigit():
+
+    if shift_input.isdigit():
+        shift = int(shift_input)
+
         while True:
-            message = input("Enter message to be encoded by adding " + str(shift) + " to each letter in UniCode: ")
+            message = input(
+                "Enter message to be encoded by shifting each letter by " + str(shift) + ": ").upper().strip()
 
-            message = message.upper().strip()
-                
             if message.isalpha():
-                for letter in message:
-                    if letter.isalpha():
-                        if letter == "Z":
-                            letter = "A"
-                        else:
-                            letter = chr(ord(letter) + int(shift))
-                        code += letter
-                print(code)
-                break
+                message_list = [ord(i) for i in message]
 
+                unicode_list = [j + shift if j + shift <= 90
+                                            else 65 + ((j + shift - 65) % 26)
+                                            for j in message_list]
+
+                print("Your coded message: " + "".join(chr(k) for k in unicode_list) + "\n")
+                time.sleep(2)
+                
+                print("\n!New word for encoding! \n\\to stop the program press Enter\\\n")
+                break
+            
             else:
-                print("Use only letters, try again below")
-                continue
+                    print("Use only letters, try again below")
+                    continue
+
     else:
-        print("Use only numbers, try again below")
+        print("Use only a single number composed of one or more digits, try again below")
         continue
